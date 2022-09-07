e95e3e6dcf370bf820e5f0c908b60a203b60736e
HirenLimbasiya <107846821+HirenLimbasiya@users.noreply.github.com>
2022-09-07 09:43:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React , { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
+  const [hstate , setHstate] = useState("Marco");
+  const [bstate , setBstate] = useState("Polo");
   
+  function change(){
+    setHstate((prevValue) => {
+      if(prevValue == "Polo"){
+        return "Marco";
+      }else{
+        return "Polo";
+      }
+    });
+    setBstate((prevValue) => {
+      if(prevValue == "Polo"){
+        return "Marco";
+      }else{
+        return "Polo";
+      }
+    });
+  }
   return (
     <div id="main">
-
+      <h1 id="marco-polo">{hstate}</h1>
+      <button id="marco-polo-toggler" onClick={change}>{bstate}</button>
     </div>
   )
 }
